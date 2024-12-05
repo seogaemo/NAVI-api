3316df5abad4692de6885da9828a9a8175b2b7ac
suk-6 <me@suk.kr>
2024-12-05 23:00:25 +0900

    feat(router): restructure main app and add root router

--- app/main.py
+++ app/main.py
@@ -3,14 +3,12 @@ from fastapi import FastAPI
 
 from utils.config import Settings
 
+from routers import root
+
 app = FastAPI()
 settings = Settings()
 
-
-@app.get("/")
-async def root():
-    return "Hello World!"
-
+app.include_router(root.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
