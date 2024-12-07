87ef32c3c331c36e054056c86907ec1f79e7bf84
suk-6 <me@suk.kr>
2024-12-07 23:40:17 +0900

    feat: implement prediction cache

--- common/utils/db.py
+++ common/utils/db.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+from api.common.dto.prediction.response import PredictionResponse
 from api.common.utils.config import Settings
 
 
@@ -17,14 +18,17 @@ class DB:
 
         self.cur = self.conn.cursor()
 
-        if self.checkTableIsExist() is False:
+        if self.checkPointTableIsExist() is False:
             raise Exception("Table 'road_data' does not exist")
+
+        if self.checkPredctionTableIsExist() is False:
+            self.createPredictionTable()
 
     def __del__(self):
         self.cur.close()
         self.conn.close()
 
-    def checkTableIsExist(self):
+    def checkPointTableIsExist(self):
         query = """
         SELECT EXISTS (
             SELECT 1
@@ -36,7 +40,30 @@ class DB:
         self.cur.execute(query)
         return self.cur.fetchone()[0]
 
-    def getNearestLocations(self, lat, lng, limit=3):
+    def checkPredctionTableIsExist(self):
+        query = """
+        SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'prediction_data'
+        )
+        """
+
+        self.cur.execute(query)
+        return self.cur.fetchone()[0]
+
+    def createPredictionTable(self):
+        query = """
+        CREATE TABLE prediction_data (
+            point_id TEXT PRIMARY KEY,
+            predictions JSONB NOT NULL
+        )
+        """
+
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def getNearestLocations(self, lat: float, lng: float, limit: int = 3):
         # 10미터 제한
         query = """
         SELECT *
@@ -57,3 +84,36 @@ class DB:
             return None
 
         return rows
+
+    def getPrediction(self, pointId: str) -> PredictionResponse | None:
+        query = """
+        SELECT predictions
+        FROM prediction_data
+        WHERE point_id = %s
+        """
+
+        self.cur.execute(query, (pointId,))
+        row = self.cur.fetchone()
+
+        if row is None:
+            return None
+
+        return row[0]
+
+    def savePrediction(self, pointId: str, predictions: PredictionResponse):
+        query = """
+        INSERT INTO prediction_data (point_id, predictions)
+        VALUES (%s, %s)
+        ON CONFLICT (point_id) DO UPDATE
+        SET predictions = %s
+        """
+
+        self.cur.execute(
+            query,
+            (
+                pointId,
+                predictions.model_dump_json(),
+                predictions.model_dump_json(),
+            ),
+        )
+        self.conn.commit()
--- services/prediction/prediction_service.py
+++ services/prediction/prediction_service.py
@@ -4,11 +4,13 @@ from pydantic import TypeAdapter
 from api.common.dto.prediction.response import PredictionResponse
 from api.common.models.object import PredictionObject
 from api.common.utils.config import Settings
+from api.common.utils.db import DB
 
 
 class Prediction:
     def __init__(self):
         self.settings = Settings()
+        self.db = DB()
 
     async def getPredtions(self, pointIds: list[str]) -> list[PredictionObject]:
         """
@@ -30,6 +32,11 @@ class Prediction:
         예측 결과를 반환하는 함수입니다.
         """
 
+        prediction = await self.getPredictionFromDB(pointId)
+
+        if prediction is not None:
+            return prediction
+
         try:
             response = requests.get(
                 f"{self.settings.PREDICTION_URL}?id={pointId}"
@@ -40,7 +47,27 @@ class Prediction:
                 raise requests.exceptions.RequestException
 
             adapter = TypeAdapter(PredictionResponse)
-            return adapter.validate_python(response.json())
+            prediction = adapter.validate_python(response.json())
+
+            await self.savePredictionToDB(pointId, prediction)
+
+            return prediction
         except requests.exceptions.RequestException as e:
             print(f"Prediction API Request Failed: {e}")
             raise
+
+    async def getPredictionFromDB(self, pointId: str) -> PredictionResponse:
+        """
+        DB에 캐싱된 예측 결과를 반환하는 함수입니다.
+        """
+
+        return self.db.getPrediction(pointId)
+
+    async def savePredictionToDB(
+        self, pointId: str, predictions: PredictionResponse
+    ):
+        """
+        DB에 예측 결과를 저장하는 함수입니다.
+        """
+
+        return self.db.savePrediction(pointId, predictions)
