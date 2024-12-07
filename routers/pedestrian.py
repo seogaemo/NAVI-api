9c93a5d72283fb8e52c5d8e1909426990ef79311
suk-6 <me@suk.kr>
2024-12-07 21:49:02 +0900

    feat: add single and multi pedestrian route endpoints

--- routers/pedestrian.py
+++ routers/pedestrian.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from api.common.models.processing import ProcessingMultiResult, ProcessingResult
 from api.services.main.main_service import MainService
 from api.common.dto.pedestrian.request import PedestrianRouteRequest
 
@@ -8,10 +9,29 @@ router = APIRouter(prefix="/pedestrian")
 main = MainService()
 
 
-@router.post("/")
-async def getPedestrianRoute(data: PedestrianRouteRequest):
+@router.post("/single")
+async def get_single_pedestrian_route(
+    data: PedestrianRouteRequest,
+) -> ProcessingResult:
     """
+    # 보행자 경로 안내 API (단일 버전)
+
     보행자 경로 안내 API를 호출하여 경로 정보를 반환하는 함수입니다.
     """
 
     return await main.getPedestrianRoute(data)
+
+
+@router.post("/multi")
+async def get_multi_pedestrian_route(
+    data: PedestrianRouteRequest,
+) -> ProcessingMultiResult:
+    """
+    # 보행자 경로 안내 API (다중 버전)
+
+    보행자 경로 안내 API를 호출하여 경로 정보를 반환하는 함수입니다.
+
+    총 3개의 경로를 반환합니다.
+    """
+
+    return "Not implemented yet"
