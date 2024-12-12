7b5352418477f7d4158f15bd1c1e995e172e2fd9
PleBea <bini060315kim@gmail.com>
2024-12-12 17:40:17 +0900

    feat: add CORS middleware to allow requests from localhost

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.common.utils.config import Settings
 
@@ -14,6 +15,17 @@ app.include_router(image.router)
 app.include_router(location.router)
 app.include_router(poi.router)
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
